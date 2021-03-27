9810e2e51c6a2d76ec9d2422b9bb24c09754d238
Max Moeschinger <m.moeschinger@gmail.com>
2021-03-27 16:07:35 +0100

    add bottleneck

--- src/request.js
+++ src/request.js
@@ -1,12 +1,21 @@
-const axios = require("axios");
+const axios = require('axios');
+const Bottleneck = require('bottleneck');
 
-module.exports = async (config, path, method, options = {}) => axios({
-    ...options,
-    url: `${config.url}${path}`,
-    method,
-    headers: {
-        ...options.headers,
-        Authorization: `Bearer ${config.apiToken}`,
-        'accept-version': '1.0.0',
-    },
+const limiter = new Bottleneck({
+    maxConcurrent: 1,
+    minTime: 1100,
 });
+
+module.exports = async (config, path, method, options = {}) =>
+    limiter.schedule(() =>
+        axios({
+            ...options,
+            url: `${config.url}${path}`,
+            method,
+            headers: {
+                ...options.headers,
+                Authorization: `Bearer ${config.apiToken}`,
+                'accept-version': '1.0.0',
+            },
+        })
+    );
